57d091eab23172a64bf70182370d632aaca541d2
Michael Sacher <michael.sacher@gmail.com>
2012-11-12 23:45:02 +0100

    generated header file for JNI and implementation skeleton

---
+++ Uebung2Java/src/ch_fhnw_ch_prcpp_uebung2schwarzsacher_Matrix.cpp
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ch_fhnw_ch_prcpp_uebung2schwarzsacher_Matrix.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/12/2012 11:32:47 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Michael Sacher (ms), michael.sacher@students.fhnw.ch
+ *         					Pascal Schwarz (ps), pascal.schwarz@students.fhnw.ch
+ *   Organization:  Fachhochschule Nordwestschweiz
+ *
+ * =====================================================================================
+ */
+
+
+#include "ch_fhnw_ch_prcpp_uebung2schwarzsacher_Matrix.h"
+
+JNIEXPORT void JNICALL Java_ch_fhnw_ch_prcpp_uebung2schwarzsacher_Matrix_multiplyC{
+
+}
+
